c25676cd1542f3a1e6782caa4dbc3148462899f5
RamvigneshA <ramvigneshnanban@gmail.com>
2024-11-11 13:47:46 +0530

    router path changed

--- src/main.jsx
+++ src/main.jsx
@@ -7,23 +7,23 @@ import { MyCart } from './myCart.jsx';
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Navigate,
   Route,
   Router,
   RouterProvider,
 } from 'react-router-dom';
 
 const router = createBrowserRouter(
-      createRoutesFromElements(
-            <Route path="/" element={<App />}>
-                  <Route path='list' element={<ShoppingList/>} />
-                  <Route path='productdetails' element={<ProductViewPage/>} />
-                  <Route path='cart' element={<MyCart/>} />
-
-            </Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<Navigate to="/list" />} />
+      <Route path="list" element={<ShoppingList />} />
+      <Route path="productdetails" element={<ProductViewPage />} />
+      <Route path="cart" element={<MyCart />} />
+    </Route>
+  )
 );
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} >
-   
-  </RouterProvider>
+  <RouterProvider router={router}></RouterProvider>
 );
